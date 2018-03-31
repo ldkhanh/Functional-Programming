b547e6da2dad07bb068113acd6747c10595a3a7c
khanhduyle <khanhduyle@assembla.com>
2018-03-31 08:46:51 -0700

    Filter

--- src/main/java/FilterExample.java
+++ src/main/java/FilterExample.java
@@ -4,5 +4,11 @@ import java.util.List;
 public class FilterExample {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Peter", "Sam", "Khanh", "Vy");
+
+        names.stream().filter(FilterExample::isNotSam).forEach(System.out::println);
+    }
+
+    private static boolean isNotSam(String name) {
+        return !name.equals("Sam");
     }
 }
