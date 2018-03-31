bb5302e9da5ec89f5c6c1835b5bcb55e95f860e5
khanhduyle <khanhduyle@assembla.com>
2018-03-31 15:53:13 -0700

    FlatMap

---
+++ src/main/java/FlatMapExample.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FlatMapExample {
+    public static void main(String[] args) {
+        List<User> users = Arrays.asList(
+                new User("Peter", 20, Arrays.asList("1","2")),
+                new User("Sam", 40, Arrays.asList("3","4","5")),
+                new User("Ryan", 60, Arrays.asList("6")),
+                new User("Khanh", 28, Arrays.asList("7","8"))
+        );
+
+        Optional<String> stringOptional = users.stream()
+                .map(user -> user.getPhoneNumbers().stream())
+                .flatMap(stringStream -> stringStream.filter(phoneNo -> phoneNo.equals("5")))
+                .findAny();
+
+        stringOptional.ifPresent(System.out::println);
+    }
+
+    static class User {
+        private String name;
+        private Integer age = 30;
+        private List<String> phoneNumbers;
+
+        public User(String name, Integer age, List<String> phoneNumbers) {
+            this.name = name;
+            this.age = age;
+            this.phoneNumbers = phoneNumbers;
+        }
+
+        public List<String> getPhoneNumbers() {
+            return phoneNumbers;
+        }
+
+        public void setPhoneNumbers(List<String> phoneNumbers) {
+            this.phoneNumbers = phoneNumbers;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+}
---
+++ src/main/java/MapperIntExample.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapperIntExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Peter", "Sam", "Khanh", "Vy");
+
+        List<User> userList = names.stream()
+                .filter(MapperIntExample::isNotSam)
+                .map(User::new)
+                .collect(Collectors.toList());
+        System.out.println(userList);
+
+        int sum = userList.stream()
+                .filter(user->!user.getName().equals("Khanh"))
+                .mapToInt(User::getAge)
+                .sum();
+        System.out.println(sum);
+
+    }
+
+    private static boolean isNotSam(String name) {
+        return !name.equals("Sam");
+    }
+
+    static class User {
+        private String name;
+        private Integer age = 30;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+}
