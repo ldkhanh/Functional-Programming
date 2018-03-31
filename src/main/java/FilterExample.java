07fd269caa2813689f0d481a1805e941679d8b96
khanhduyle <khanhduyle@assembla.com>
2018-03-31 08:16:57 -0700

    First Commit

---
+++ src/main/java/FilterExample.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Peter", "Sam", "Khanh", "Vy");
+    }
+}
